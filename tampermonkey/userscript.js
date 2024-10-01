d10e66ed3edb062fb1d079925d6e1d7c643b6bf4
boobie <boobie@riseup.net>
2024-10-01 05:16:17 -0400

    taky@1727774177

--- tampermonkey/userscript.js
+++ tampermonkey/userscript.js
@@ -1,7 +1,6 @@
 // ==UserScript==
-// @name         Google Mirror Page Cleaner
-// @namespace    prixi
-// @description  Clean up Google search results and modify the search box
+// @name         (prixi) google mirror helper
+// @description  clean up search results
 // @version      1.1
 // @author       <taky@taky.com>
 // @match        http://localhost:9001/*
@@ -13,7 +12,7 @@
 (function() {
   'use strict';
 
-  // Immediately add CSS to the head of the document to hide unwanted elements
+  // immediately add css to the head of the document to hide unwanted elements
   const cssHideRule = `
     div.Efnghe,
     [aria-label="Ads"],
@@ -39,6 +38,7 @@
   document.head.appendChild(styleSheet);
 
   function replaceGoogleLogo() {
+    return;
     const base64SVG = 'CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjcyIiBoZWlnaHQ9IjkyIj48cGF0aCBmaWxsPSIjMTExMTExIiBkPSJNMTE1Ljc1IDQ3LjE4YzAgMTIuNzctOS45OSAyMi4xOC0yMi4yNSAyMi4xOHMtMjIuMjUtOS40MS0yMi4yNS0yMi4xOEM3MS4yNSAzNC4zMiA4MS4yNCAyNSA5My41IDI1czIyLjI1IDkuMzIgMjIuMjUgMjIuMTh6bS05Ljc0IDBjMC03Ljk4LTUuNzktMTMuNDQtMTIuNTEtMTMuNDRTODAuOTkgMzkuMiA4MC45OSA0Ny4xOGMwIDcuOSA1Ljc5IDEzLjQ0IDEyLjUxIDEzLjQ0czEyLjUxLTUuNTUgMTIuNTEtMTMuNDR6Ii8+PHBhdGggZmlsbD0iIzExMTExMSIgZD0iTTE2My43NSA0Ny4xOGMwIDEyLjc3LTkuOTkgMjIuMTgtMjIuMjUgMjIuMThzLTIyLjI1LTkuNDEtMjIuMjUtMjIuMThjMC0xMi44NSA5Ljk5LTIyLjE4IDIyLjI1LTIyLjE4czIyLjI1IDkuMzIgMjIuMjUgMjIuMTh6bS05Ljc0IDBjMC03Ljk4LTUuNzktMTMuNDQtMTIuNTEtMTMuNDRzLTEyLjUxIDUuNDYtMTIuNTEgMTMuNDRjMCA3LjkgNS43OSAxMy40NCAxMi41MSAxMy40NHMxMi41MS01LjU1IDEyLjUxLTEzLjQ0eiIvPjxwYXRoIGZpbGw9IiMxMTExMTEiIGQ9Ik0yMDkuNzUgMjYuMzR2MzkuODJjMCAxNi4zOC05LjY2IDIzLjA3LTIxLjA4IDIzLjA3LTEwLjc1IDAtMTcuMjItNy4xOS0xOS42Ni0xMy4wN2w4LjQ4LTMuNTNjMS41MSAzLjYxIDUuMjEgNy44NyAxMS4xNyA3Ljg3IDcuMzEgMCAxMS44NC00LjUxIDExLjg0LTEzdi0zLjE5aC0uMzRjLTIuMTggMi42OS02LjM4IDUuMDQtMTEuNjggNS4wNC0xMS4wOSAwLTIxLjI1LTkuNjYtMjEuMjUtMjIuMDkgMC0xMi41MiAxMC4xNi0yMi4yNiAyMS4yNS0yMi4yNiA1LjI5IDAgOS40OSAyLjM1IDExLjY4IDQuOTZoLjM0di0zLjYxaDkuMjV6bS04LjU2IDIwLjkyYzAtNy44MS01LjIxLTEzLjUyLTExLjg0LTEzLjUyLTYuNzIgMC0xMi4zNSA1LjcxLTEyLjM1IDEzLjUyIDAgNy43MyA1LjYzIDEzLjM2IDEyLjM1IDEzLjM2IDYuNjMgMCAxMS44NC01LjYzIDExLjg0LTEzLjM2eiIvPjxwYXRoIGZpbGw9IiMxMTExMTEiIGQ9Ik0yMjUgM3Y2NWgtOS41VjNoOS41eiIvPjxwYXRoIGZpbGw9IiMxMTExMTEiIGQ9Im0yNjIuMDIgNTQuNDggNy41NiA1LjA0Yy0yLjQ0IDMuNjEtOC4zMiA5LjgzLTE4LjQ4IDkuODMtMTIuNiAwLTIyLjAxLTkuNzQtMjIuMDEtMjIuMTggMC0xMy4xOSA5LjQ5LTIyLjE4IDIwLjkyLTIyLjE4IDExLjUxIDAgMTcuMTQgOS4xNiAxOC45OCAxNC4xMWwxLjAxIDIuNTItMjkuNjUgMTIuMjhjMi4yNyA0LjQ1IDUuOCA2LjcyIDEwLjc1IDYuNzIgNC45NiAwIDguNC0yLjQ0IDEwLjkyLTYuMTR6bS0yMy4yNy03Ljk4IDE5LjgyLTguMjNjLTEuMDktMi43Ny00LjM3LTQuNy04LjIzLTQuNy00Ljk1IDAtMTEuODQgNC4zNy0xMS41OSAxMi45M3oiLz48cGF0aCBmaWxsPSIjMTExMTExIiBkPSJNMzUuMjkgNDEuNDFWMzJINjdjLjMxIDEuNjQuNDcgMy41OC40NyA1LjY4IDAgNy4wNi0xLjkzIDE1Ljc5LTguMTUgMjIuMDEtNi4wNSA2LjMtMTMuNzggOS42Ni0yNC4wMiA5LjY2QzE2LjMyIDY5LjM1LjM2IDUzLjg5LjM2IDM0LjkxLjM2IDE1LjkzIDE2LjMyLjQ3IDM1LjMuNDdjMTAuNSAwIDE3Ljk4IDQuMTIgMjMuNiA5LjQ5bC02LjY0IDYuNjRjLTQuMDMtMy43OC05LjQ5LTYuNzItMTYuOTctNi43Mi0xMy44NiAwLTI0LjcgMTEuMTctMjQuNyAyNS4wMyAwIDEzLjg2IDEwLjg0IDI1LjAzIDI0LjcgMjUuMDMgOC45OSAwIDE0LjExLTMuNjEgMTcuMzktNi44OSAyLjY2LTIuNjYgNC40MS02LjQ2IDUuMS0xMS42NWwtMjIuNDkuMDF6Ii8+PC9zdmc+Cg==';
     const logoContainer = document.querySelector('.k1zIA.rSk4se');
     if (logoContainer) {
@@ -65,7 +65,7 @@
       'div.NDnoQ > div.Q3DXx'
     ];
 
-    // Perform removal of elements after the DOM has been fully parsed
+    // perform removal of elements after the dom has been fully parsed
     elementsToRemove.forEach(selector => {
       document.querySelectorAll(selector).forEach(element => {
         element.style.display = 'none';
@@ -97,7 +97,7 @@
     replaceGoogleLogo();
   }
 
-  // Run modifications as soon as the DOM is ready
+  // run modifications as soon as the dom is ready
   if (document.readyState === 'interactive' || document.readyState === 'complete') {
     runAllModifications();
   } else {
